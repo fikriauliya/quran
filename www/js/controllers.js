6e83a23075545eda7ff4c9fc3b29b9db6ebbf74a
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-05-26 22:20:32 +0800

    Download all Quran pages, display the progress, store the progress in DB for avoid redownloading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('quran.controllers', [])
+angular.module('quran.controllers', ['ionic'])
 
 .controller('AppCtrl', function($scope) {
 })
@@ -17,31 +17,77 @@ angular.module('quran.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('PagesCtrl', function($scope, $state, SurahTextServices) {
+.controller('PagesCtrl', function($scope, $state, $ionicPopup, SurahTextServices) {
   $scope.surah = SurahTextServices.getAllSurah();
   $scope.go = function(cur_surah) {
     $state.go('app.page', {'pageNo':cur_surah.page})
   }
 
-  if (typeof(FileTransfer) === "function") { 
-    var fileTransfer = new FileTransfer();
-    var uri = encodeURI("https://raw.githubusercontent.com/fikriauliya/quran_resources/master/images/1.png");
-    var fileURL = 'file:///storage/emulated/0/quran/1.png'
+  var popup = $ionicPopup.show({
+    templateUrl: 'templates/download-modal.html',
+    title: 'Downloading...',
+    scope: $scope,
+    buttons: [
+      { 
+        text: 'Cancel',
+        type: 'button-positive',
+        onTap: function(e){
+          return "Canceled";
+        }
+      }
+    ]
+  });
+  // popup.then(function(res) {
+  // });
 
-    window.alert("Start download");
-    fileTransfer.download(
-        uri,
-        fileURL,
-        function(entry) {
-            window.alert("download complete: " + entry.fullPath);
-        },
-        function(error) {
-            window.alert("download error source " + error.source);
-            window.alert("download error target " + error.target);
-            window.alert("upload error code" + error.code);
-        },
-        false);
-    window.alert("Finish download");
+  $scope.progress = 0;
+  $scope.downloaded_count = 0;
+
+  $scope.startDownload = function() {
+    var indexes = [];
+    for (var i = 1; i <= 604; i++) {
+      indexes.push(i);
+    };
+    
+    async.eachSeries(indexes, function(i, callback){
+      is_downloaded = localStorage.getItem("page_" + i + "_downloaded");
+      if (is_downloaded === null) {
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("https://raw.githubusercontent.com/fikriauliya/quran_resources/master/images_small/" + i + ".png");
+        var fileURL = 'file:///storage/emulated/0/quran/' + i + '.png';
+
+        fileTransfer.download(
+          uri,
+          fileURL,
+          function(entry) {            
+            localStorage.setItem("page_" + i + "_downloaded", true);
+            $scope.$apply(function() {
+              $scope.downloaded_count = $scope.downloaded_count + 1;
+              $scope.progress = $scope.downloaded_count * 100 / 604;
+            });
+            callback();
+          },
+          function(error) {
+            callback(error);
+          },
+          false);
+      } else {
+        $scope.downloaded_count = $scope.downloaded_count + 1;
+        callback();
+      }
+    }, function(err){
+      if (err) {
+        window.alert("Error: " + err);
+      } else {
+        popup.close();
+      }
+    });
+  }
+
+  if (typeof(FileTransfer) === "function") { 
+    $scope.startDownload();
+  } else {
+    console.log("FileTransfer function is undefined");
   }
 })
 
