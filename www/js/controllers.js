812b522d87c47e2465d7067c064fe40b192291c5
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-05-27 21:21:11 +0800

    Enable cancelling the download

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,6 +23,8 @@ angular.module('quran.controllers', ['ionic'])
     $state.go('app.page', {'pageNo':cur_surah.page})
   }
 
+  var stopDownload = false;
+
   var popup = $ionicPopup.show({
     templateUrl: 'templates/download-modal.html',
     title: 'Downloading...',
@@ -32,13 +34,16 @@ angular.module('quran.controllers', ['ionic'])
         text: 'Cancel',
         type: 'button-positive',
         onTap: function(e){
+          stopDownload = true;
           return "Canceled";
         }
       }
     ]
   });
-  // popup.then(function(res) {
-  // });
+  popup.then(function(res) {
+    console.log("Popup closed");
+    console.log(res);
+  });
 
   $scope.progress = 0;
   $scope.downloaded_count = 0;
@@ -49,53 +54,54 @@ angular.module('quran.controllers', ['ionic'])
 
     function onInitFs(fileSystem) {
       fileSystem.root.getDirectory('quran_resources', {create: true}, function(dirEntry) {
-        // console.log(fileSystem.root.fullPath);
         console.log(dirEntry.toURL());
+
         var indexes = [];
         for (var i = 1; i <= 604; i++) {
           indexes.push(i);
         };
 
         async.eachSeries(indexes, function(i, callback){
-          is_downloaded = localStorage.getItem("page_" + i + "_downloaded");
-          if (is_downloaded === null) {
-            var fileTransfer = new FileTransfer();
-            var uri = encodeURI("https://raw.githubusercontent.com/fikriauliya/quran_resources/master/images_small/" + i + ".png");
-            var fileURL = dirEntry.toURL() + '/quran/' + i + '.png';
+          if (stopDownload != true) {
+            is_downloaded = localStorage.getItem("page_" + i + "_downloaded");
+            if (is_downloaded === null) {
+              var fileTransfer = new FileTransfer();
+              var uri = encodeURI("https://raw.githubusercontent.com/fikriauliya/quran_resources/master/images_small/" + i + ".png");
+              var fileURL = dirEntry.toURL() + '/quran/' + i + '.png';
 
-            console.log(i);
-            fileTransfer.download(
-              uri,
-              fileURL,
-              function(entry) {            
-                localStorage.setItem("page_" + i + "_downloaded", true);
-                $scope.$apply(function() {
-                  $scope.downloaded_count = $scope.downloaded_count + 1;
-                  $scope.progress = $scope.downloaded_count * 100 / 604;
-                });
-                callback();
-              },
-              function(error) {
-                callback(error);
-              },
-              false);
-          } else {
-            $scope.downloaded_count = $scope.downloaded_count + 1;
-            callback();
+              console.log(i);
+              fileTransfer.download(
+                uri,
+                fileURL,
+                function(entry) {            
+                  localStorage.setItem("page_" + i + "_downloaded", true);
+                  $scope.$apply(function() {
+                    $scope.downloaded_count = $scope.downloaded_count + 1;
+                    $scope.progress = $scope.downloaded_count * 100 / 604;
+                  });
+                  callback();
+                },
+                function(error) {
+                  callback(error);
+                },
+                false);
+            } else {
+              $scope.downloaded_count = $scope.downloaded_count + 1;
+              callback();
+            }
           }
         }, function(err){
-          if (err) {
-            window.alert("Error: " + err);
+          popup.close();
+          if (err && err != "Download stopped") {
+            window.alert("An error occured while downloading. Plaese make sure the Internet is on & there is enough space in memory");
             console.log(err);
-          } else {
-            popup.close();
           }
         });
       }, errorHandler);
     }
 
     function errorHandler(message) {
-      window.alert(message);
+      window.alert("An error occured while storing the image. Please make sure there is enough space in memory");
       console.log(message);
     }
   }
