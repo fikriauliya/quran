c1ce8e2bb9cfebf0704509895d3c89d5c059e0d6
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-05-28 21:39:48 +0800

    Remove left over from generated base code

--- www/js/app.js
+++ www/js/app.js
@@ -50,33 +50,14 @@ angular.module('quran', ['ionic', 'quran.controllers', 'quran.services'])
       }
     })
 
-    .state('app.browse', {
-      url: "/browse",
+    .state('app.search', {
+      url: "/search",
       views: {
         'menuContent' :{
-          templateUrl: "templates/browse.html"
+          templateUrl: "templates/search.html"
         }
       }
     })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/pages');
 });
